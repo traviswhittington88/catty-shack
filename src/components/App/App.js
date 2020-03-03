4024f37ea37b8947e4ed28d1eb04d0b9f318e1ea
traviswhittington88 <traviswhittington@ymail.com>
2020-03-02 22:32:14 -0600

    styled landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PrivateRoute from '../Utils/PrivateRoute'
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
 import HomePage from '../../routes/HomePage/HomePage'
 import LoginPage from '../../routes/LoginPage/LoginPage'
 import SignupPage from '../../routes/SignupPage/SignupPage'
 import LandingPage from '../../routes/LandingPage/LandingPage'
-
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
 import './App.css';
 
-function App() {
-  return (
-    <main className='App'>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/signup" component={SignupPage} />
-          <Route exact path="/landing" component={LandingPage} />
-        </Switch>
-      </Router>
-      <h1>Our App</h1>
-    </main>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props)
 
-export default App;
+    this.state = {
+      hasError: false,
+      error: { message: null }
+    }
+  }
+ 
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App__header'></header>
+        <main className='App__main'>
+        {this.state.hasError && <p className='errorText'>{this.state.err}</p>}
+          <Router>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/home" component={HomePage} />  {/* make private later */}
+              <PublicOnlyRoute exact path="/login" component={LoginPage} />
+              <PublicOnlyRoute exact path="/signup" component={SignupPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+            <Footer />
+          </Router>
+        </main>
+      </div>
+    );
+  }
+}
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import TokenService from '../../services/token-service'
+import './Nav.css'
+import logo from '../../images/logo.png'
 
-export class Nav extends Component {
-  render() {
+
+export default class Nav extends Component {
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken()
+  }
+
+  renderLogoutLink() {
     return (
-      <div>
-        
-      </div>
+        <div className='item logout'>
+          <Link
+            onClick={this.handleLogoutClick}
+            to='/'>
+            Logout
+          </Link>
+        </div>
     )
   }
-}
 
-export default Nav
+  renderLoginLink() {
+    return (
+      <>
+        <div className='item signup'>
+          <Link
+            to='/signup'>
+            Sign up
+          </Link>
+        </div>
+        <div className='item login'>
+          <Link
+            to='/login'>
+            Login
+          </Link>
+        </div>
+      </>
+    )
+  }
+
+  render() {
+      return (
+        <nav role="navigation" className="nav bg-light">
+          <div className="item name">
+            <Link
+              to='/homepage'
+              className="title"
+            >
+              <img src={logo} alt="catty shack logo" className="logo"></img>
+            </Link> 
+          </div>
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </nav>
+      )
+  }
+}
