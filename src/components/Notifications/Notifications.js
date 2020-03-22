796179de1bac228b4d15cee9f1b2fcdd7eb88dfe
traviswhittington88 <traviswhittington@ymail.com>
2020-03-22 16:46:35 -0500

    changed bio

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 import AppContext from '../../contexts/appContext';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+// MUI Stuff
+import Menu from '@material-ui/core/Menu';
+import Menu from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Menu from '@material-ui/core/Menu';
+import Menu from '@material-ui/core/Menu';
 
 class Notifications extends Component {
+  state = {
+    anchorEl = null
+  }
   render() {
     return <div></div>;
   }
